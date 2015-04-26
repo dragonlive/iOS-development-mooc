e906965489af24ba6c11ebdcead5b1bd263c03db
Longsheng Wang <longsheng@retentionscience.com>
2015-04-25 22:14:55 -0700

    calculator almost done

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    @IBOutlet weak var history: UILabel!
+    
     var userInTyping: Bool = false
     
     @IBAction func appendDigit(sender: UIButton) {
@@ -24,14 +26,27 @@ class ViewController: UIViewController {
             userInTyping = true
         }
     }
+    
     @IBAction func operate(sender: UIButton) {
+        appendToHistory(sender.currentTitle!)
         switch sender.currentTitle! {
         case "+": performOperation({ $0 + $1 })
         case "−": performOperation({ $1 - $0 })
         case "×": performOperation({ $0 * $1 })
         case "÷": performOperation({ $1 / $0 })
+        case "√": performOperation { sqrt($0)}
+        case "π": performOperation { $0 * M_PI}
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "c": history.text = ""; operandStack.removeAll(); display.text="0"; break;
         default: break;
         }
+    }
+    @IBAction func appendDot() {
+        if display.text?.rangeOfString(".") == nil {
+            display.text = display.text! + "."
+        }
+        userInTyping = true
     }
     
     func performOperation(operation:(Double, Double) -> Double){
@@ -40,12 +55,29 @@ class ViewController: UIViewController {
             enter()
         }
     }
+    
+    private func performOperation(operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
 
     var operandStack = Array<Double>()
     @IBAction func enter() {
         operandStack.append(displayValue)
         userInTyping = false
+        appendToHistory(displayValue)
         println("\(operandStack)")
+    }
+    
+    func appendToHistory<T>(content: T){
+        if history.text != nil && !history.text!.isEmpty {
+            history.text = history.text!  + "\r\n" + "\(content)"
+        }
+        else {
+            history.text = "\(content)"
+        }
     }
     
     var displayValue: Double{
---
+++ GuidedTour.playground/section-5.swift
@@ -0,0 +1,3 @@
+var myVariable = 43
+myVariable = 50
+let myConstant = 42
